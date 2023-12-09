f2af9d38d3da700369491c0a5a7ee8d2b495e1cf
Nozom <nozomrec@gmail.com>
2023-12-09 08:16:53 +0200

    add hashing password in the user model

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcrypt')
 const userSchema = new mongoose.Schema({
   username: {
     type: "String",
@@ -13,6 +14,25 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
 });
+
+const saltRounds = 10;
+
+userSchema.pre('save', function (next) {
+  const user = this;
+
+  // Only hash the password if it has been modified or is new
+  if (!user.isModified("password")) return next();
+
+  // Hash the password
+  bcrypt.hash(user.password, saltRounds, (err, hash) => {
+    if (err) return next(err);
+
+    // Replace the plaintext password with the hashed password
+    user.password = hash;
+    next();
+  });
+})
+
 
 const User = mongoose.model("User", userSchema);
 
